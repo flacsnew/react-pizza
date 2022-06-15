14ccff76d8dda22f6c6f2635a1156da9435058da
chislov <chislov.vu@kvant-telecom.ru>
2022-06-15 10:57:44 +0300

    Skeleton loading added

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
+import Placeholder from './components/PizzaBlock/placeholder';
 
 function App() {
   let [pizzas, setPizzas] = React.useState([]);
+  let [isLoading, setLoading] = React.useState(true);
   // onMount
   React.useEffect(() => {
     fetch('https://62a96da03b3143855432f30f.mockapi.io/items')
       .then(response => response.json())
-      .then(arr => { setPizzas(arr); });
+      .then(arr => { setLoading(false); setPizzas(arr); });
   }, []);
 
   return (
@@ -25,7 +27,11 @@ function App() {
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-            {pizzas.map((object) => <PizzaBlock key={object.id} {...object} />)}
+            {
+              isLoading
+                ? [... new Array(10)].map((_, index) => <Placeholder key={index} />)
+                : pizzas.map((object) => <PizzaBlock key={object.id} {...object} />)
+            }
           </div>
         </div>
       </div>
